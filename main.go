8e8f20e56dd07923ac46570cbdf4ef77936303a1
ngundangin <ngundanginnet@gmail.com>
2022-06-18 19:27:31 +0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,101 @@
+package main
+
+func main() {
+	//var one, two, three = 1, 2, 3
+	//var penjumlahan = one + two + three
+	//
+	//name := new(string)
+	//const menjadikan = "ini adalah penjumlahan"
+	//print(one, penjumlahan, " ", menjadikan, name)
+	//
+	//	var isBoolean = true
+	//	print(isBoolean)
+	//
+	//var neString = `ini adalah string
+	//yang banyak baris`
+	//print(neString)
+	//aritmatika golang
+	//var pembagian = 10 / 3
+	//var pengurangan = 10 - 3
+	//var penjumlahan = 10 + 3
+	//var pemperpangkatan = 10 % 3
+	//print(pembagian, ".", pengurangan, ".", penjumlahan, ".", pemperpangkatan)
+	//perbandingan golang
+	//var a = 10
+	//var b = 3
+	//var c = a > b
+	//var d = a < b
+	//var e = a == b
+	//var f = a != b
+	//var g = a >= b
+	//var h = a <= b
+	//print(c, d, e, f, g, h)
+	//operator golang
+	//var a = 10
+	//var b = 3
+	//var c = a + b
+	//var d = a - b
+	//var e = a * b
+	//var f = a / b
+	//var g = a % b
+	//print(c, d, e, f, g)
+	//operator logika golang
+	//var a = 10
+	//if a != 5 {
+	//	print("a lebih besar dari 5")
+	//} else if a < 5 {
+	//	print("a lebih kecil dari 5")
+	//} else {
+	//	print("a sama dengan 5")
+	//}
+	//variable temporary golang
+	//var a = 100
+	//switch a {
+	//case 10:
+	//	print("a sama dengan 10")
+	//case 9:
+	//	print("a lebih kecil dari 10")
+	//default:
+	//	print("a lebih besar dari 10")
+	//}
+	//perulangan golang
+	//for i := 0; i < 5; i++ {
+	//	if i%2 == 0 {
+	//		println(i, "genap")
+	//	} else {
+	//		println(i, "ganjil")
+	//	}
+	//}
+	//perulangan bersarang
+	//for i := 0; i < 5; i++ {
+	//	for j := i; j < 5; j++ {
+	//		//for k := j; k < 5; k++ {
+	//		print(j, " ")
+	//		//}
+	//	}
+	//	fmt.Println()
+	//}
+	//perulangan bersarang dengan break
+	//for i := 0; i < 5; i++ {
+	//	for j := i; j < 5; j++ {
+	//		if j == 3 {
+	//			break
+	//		}
+	//		print("*", " ")
+	//	}
+	//	fmt.Println()
+	//}
+	//array golang
+	//var array = [5]int{1, 2, 3, 4, 5}
+	//	looping array
+	//for i := 0; i < len(array); i++ {
+	//	println("Aku adalah nomor", array[i])
+	//}
+	//print(array[0], array[1], array[2], array[3], array[4])
+	//var names = [5]string{"Budi", "Budi", "Budi", "Budi", "Budi"}
+	//print(names[0], names[1], names[2], names[3], names[4])
+	//looping array
+	//var randomArray = [...]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	//print(randomArray[0], randomArray[1], randomArray[2], randomArray[3], randomArray[4], randomArray[5], randomArray[6], randomArray[7], randomArray[8], randomArray[9])
+
+}
